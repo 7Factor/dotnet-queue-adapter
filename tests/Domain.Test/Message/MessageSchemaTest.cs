c1fffd498855ccb0b6e9271e808a3f60304aed21
Jeremy Wood <jeremy.wood@7factor.io>
2021-12-09 16:51:11 -0500

    Add tests for MessageSchema.

---
+++ tests/Domain.Test/Message/MessageSchemaTest.cs
@@ -0,0 +1,46 @@
+using Queue.Message;
+using Xunit;
+
+namespace Queue.Test.Message;
+
+public class MessageSchemaTest
+{
+    private static readonly MessageSchema SomeSchema = new("SomeSchema");
+
+    [Fact]
+    public void MessageType_StaticInitialization_DoesNotThrowException()
+    {
+        // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+        MessageSchema.Unknown.ToString();
+    }
+
+    [Fact]
+    public void FromString_WithRandomString_ReturnsUnknownMessageType()
+    {
+        Assert.Equal(MessageSchema.Unknown, MessageSchema.FromString("asdfasdfav"));
+    }
+
+    [Fact]
+    public void FromString_WithEmptyString_ReturnsUnknownMessageType()
+    {
+        Assert.Equal(MessageSchema.Unknown, MessageSchema.FromString(""));
+    }
+
+    [Fact]
+    public void FromString_WithNull_ReturnsUnknownMessageType()
+    {
+        Assert.Equal(MessageSchema.Unknown, MessageSchema.FromString(null));
+    }
+
+    [Fact]
+    public void FromString_WithKnownMessageTypeToString_ReturnsSameMessageType()
+    {
+        Assert.Equal(SomeSchema, MessageSchema.FromString(SomeSchema.ToString()));
+    }
+
+    [Fact]
+    public void ToString_WithKnownMessageType_ReturnsKnownString()
+    {
+        Assert.Equal("SomeSchema", SomeSchema.ToString());
+    }
+}
