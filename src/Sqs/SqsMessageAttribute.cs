3a34cfcc738c0004a04d714c5746aa679376009a
Jeremy Wood <jeremy.wood@7factor.io>
2021-12-29 09:27:27 -0500

    Fix message type for messageSchema.

--- src/Sqs/SqsMessageAttribute.cs
+++ src/Sqs/SqsMessageAttribute.cs
@@ -2,7 +2,7 @@ namespace _7Factor.QueueAdapter.Sqs;
 
 internal class SqsMessageAttribute
 {
-    public static readonly SqsMessageAttribute MessageType = new("messageSchema", "string");
+    public static readonly SqsMessageAttribute MessageType = new("messageSchema", "String");
 
     private SqsMessageAttribute(string key, string type)
     {
